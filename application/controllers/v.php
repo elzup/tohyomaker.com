16928fef888b5a1b5a1c3fbe861bb8ee4286344e
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-02-14 00:47:49 +0900

    struct

---
+++ application/controllers/v.php
@@ -0,0 +1,16 @@
+<?php
+
+class V extends CI_Controller
+{
+	/* @var $_survey Survey */
+	private $_survey;
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function Index()
+    {
+        echo "this is vote page";
+    }
+}
