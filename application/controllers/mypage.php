2a59e482058fe7273aea1852ea2e3090fefcdf1c
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-02-17 22:01:16 +0900

    add controllers

---
+++ application/controllers/mypage.php
@@ -0,0 +1,13 @@
+<?php
+
+class Mypage extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	function index()
+	{
+	}
+}
---
+++ application/controllers/user.php
@@ -0,0 +1,13 @@
+<?php
+
+class User extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	function index()
+	{
+	}
+}
