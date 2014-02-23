a14ac20b8bac8ed094882d06d6e9074350d8fb67
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-02-24 05:21:14 +0900

    make.php fix open a path

--- application/views/foot.php
+++ application/views/foot.php
@@ -2,10 +2,10 @@
 			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
       <script src="https://code.jquery.com/jquery.js"></script>
       <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script type="text/javascript" src="<?=site_url()?>/lib/less-1.3.3.min.js"></script>
-      <script src="<?= site_url()?>/lib/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+      <script type="text/javascript" src="<?=base_url('lib/less-1.3.3.min.js')?>"></script>
+      <script src="<?= base_url('lib/bootstrap/js/bootstrap.min.js')?>" type="text/javascript"></script>
 			<?php if (!empty($jss)) {foreach ($jss as $js) {?>
-			<script src="<?= site_url()?>/js/{$js}.js" type="text/javascript"></script>
+			<script src="<?= base_url("js/{$js}.js")?>" type="text/javascript"></script>
 			<?php }}?>
   </body>
 </html>
--- application/views/head.php
+++ application/views/head.php
@@ -9,8 +9,9 @@
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0/build/cssreset/reset-min.css" />
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" charset="UTF-8" href="<?= site_url()?>/lib/bootstrap/css/bootstrap.min.css" media="screen" />
-    <link rel="stylesheet/less" charset="UTF-8" type="text/css" href="<?=site_url().'style/' . (isset($less_name) ? $less_name : 'main') . '.less'?>" media="screen">
+    <link rel="stylesheet" charset="UTF-8" href="<?= base_url('lib/bootstrap/css/bootstrap.min.css')?>" media="screen" />
+		<?php $less_filename = 'style/' . (isset($less_name) ? $less_name : 'main') . '.less';?>
+    <link rel="stylesheet/less" charset="UTF-8" type="text/css" href="<?=base_url($less_filename)?>" media="screen">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script> <![endif]-->
