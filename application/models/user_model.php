0eb0364d10e7890f0ce763ef951d685b80b2e348
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-02-26 23:31:47 +0900

    update user_model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,13 +3,29 @@
 class User extends CI_Model
 {
 
+	public $isLogin;
+
 	function __construct()
 	{
 		parent::__construct();
 	}
 
-	function checkLogin () {
-		$
+	function checkLogin()
+	{
+		$this->config->load('my_twitter');
+		$twitter_config = $this->config->item('TWITTER_CONSUMER');
+
+		$access_token = $_SESSION['access_token'];
+
+		if (!empty($access_token))
+		{
+			$connection = new TwitterOAuth($twitter_config['key'], $twitter_config['secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
+			$this->isLogin = true;
+		} else
+		{
+			$this->isLogin = false;
+			return false;
+		}
 	}
 
 }
