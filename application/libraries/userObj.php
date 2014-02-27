da57dd9c985a582fe4e36eb7151d22cffb7c2d38
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-02-28 01:45:22 +0900

    update user obj

--- application/libraries/userObj.php
+++ application/libraries/userObj.php
@@ -6,18 +6,32 @@ class UserObj {
 	public $screen_name;
 	public $id_twitter;
 	public $img_url;
-	pbulci $state;
+	public $state;
 
-	public function __construct()
+	public function __construct($id = null, $screen_name = null, $id_twitter = null, $img_url = null, $state = null)
 	{
-		;
+		$this->id = $id;
+		$this->screen_name = $screen_name;
+		$this->id_twitter = $id_twitter;
+		$this->img_url = $img_url;
+		$this->state = $state;
 	}
 
-	public function getArray($is_table = false) {
-		$array = arra(
+	/**
+	 * return associative array formated from objects fields
+	 * @param bool $is_table is only mysql table parameter or full
+	 * @return array 
+	 */
+	public function getDataArray($is_table = true) {
+		$array = array(
 				'id_user' => $this->id,
-				'id_tale' => $this->id,
-				'id_user' => $this->id,
-				)
+				'id_twitter' => $this->id_twitter,
+				'state' => $this->state,
+		);
+		if (!is_table) {
+			$array['img_url'] = $this->img_url;
+			$array['screen_name'] = $this->screen_name;
+		}
+		return $array;
 	}
 }
