bd8e49a14bb6165f0d7df45adb5b66a557587e7e
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-02-28 10:08:40 +0900

    update functions add function 'jump' 'set_token' 'check_token'

--- application/helpers/functions.php
+++ application/helpers/functions.php
@@ -4,10 +4,43 @@
  * application/helpers/functions.php
  */
 
-if ( !function_exists('jump'))
+if (!function_exists('jump'))
 {
-	function jump($href, $request = null, array $args = null) 
-	{
 
+	function jump($path, $parameters = null)
+	{
+		$url = $path . "?" . (empty($parameters) ? "" : http_build_query($parameters));
+		header('Location: ' . $url);
+		exit;
 	}
+
+}
+
+
+
+if (!function_exists('set_token'))
+{
+
+	function set_token()
+	{
+		$token = sha1(uniqid(mt_rand(), true));
+		$_SESSION['token'] = $token;
+		return $token;
+	}
+
+}
+
+if (!function_exists('check_token'))
+{
+
+	function check_token()
+	{
+		$token = $_POST['token'];
+		if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token']))
+		{
+			echo "不正なPOST";
+		}
+		return $token;
+	}
+
 }
