45d6e814ead75560331fbf0d59228f0ed0aa2fca
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-03-06 18:48:46 +0900

    create surveyhead,surveyselectform files

---
+++ application/views/surveyhead.php
@@ -0,0 +1,43 @@
+
+			<div class="container">
+				<div class="well">
+					<div class="row">
+						<div id="title-survey-div" class="col-sm-10">
+							<h1 id="title-survey">きのこたけのこ対戦</h1>
+						</div>
+						<div class ="col-sm-2" id="pagetype">投票ページ</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6" id="tagbox-div">
+							<p>
+								<button type="button" class="btn btn-default">default</button>
+								<button type="button" class="btn btn-default btn-tag">きのこ</button>
+								<button type="button" class="btn btn-default btn-tag">たけのこ</button>
+								<button type="button" class="btn btn-default btn-tag">お菓子</button>
+								<button type="button" class="btn btn-default btn-tag">定番</button>
+								<button type="button" class="btn btn-default btn-tag">二択</button>
+							</p>
+						</div>
+						<div class="col-sm-3" id="timestamp">2012/02/10 12:00</div>
+						<div class="col-sm-3" id="owner-name">
+							<p>
+								<button class="btn btn-default btn-owner-name">作者@arzzup</button>
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12" id="survey-description">
+							きのこの山とたけのこの里好きな方に投票して下さい。<br />
+							二行目のテキスト
+						</div>
+					</div>
+				</div>
+				<div class="row" id="pager">
+					<div class="col-sm-4 col-sm-offset-8">
+						<div class="btn-group btn-group-justified">
+							<a href="./vote.html" class="btn btn-default disabled"><i class="glyphicon glyphicon-import"></i>投票</a>
+							<a href="./view.html" class="btn btn-default"><i class="glyphicon glyphicon-stats"></i>結果</a>
+						</div>
+					</div>
+				</div>
+			</div>
---
+++ application/views/surveyselectform.php
@@ -0,0 +1,24 @@
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12" id="items-div">
+						<div class="itembox-div" data-toggle="buttons-radio">
+							<ul>
+								<li><button type="button" id="item1" class="btn btn-lg btn-block btn-default">きのこ</button></li>
+								<li><button type="button" id="item2" class="btn btn-lg btn-block btn-default">たけのこ</button></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<form action="" method="POST">
+						<div class="col-sm-8" id="text-div">
+							<textarea name="vote-text" id="vote-textarea" rows="3"></textarea>
+
+						</div>
+						<div class="col-sm-4" id="submit-div">
+							<input type="hidden" id="vote-value" name="vote-value" />
+							<button type="submit" class="btn btn-lg btn-default">投票する</button>
+						</div>
+					</form>
+				</div>
+			</div>
