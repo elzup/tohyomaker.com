ea56dcee701d0f035d603b56a5feef0e0ffc879d
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-03-09 17:48:07 +0900

    create text_helper

---
+++ application/helpers/text_helper.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!function_exists('totext_voted'))
+{
+	function totext_voted($itemname)
+	{
+		return "{$itemname} に投票しました。";
+	}
+
+}
+
+if (!function_exists('totext_share'))
+{
+	function totext_share($itemname, $survey_title, $url)
+	{
+		return "「{$itemname} 」に投票しました : {$survey_title} {$url}";
+	}
+}
