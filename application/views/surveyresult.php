6d0adb131d8631d00bdb3540d16d8aed6026093d
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-03-12 23:28:04 +0900

    udpate surveyresult.php

--- application/views/surveyresult.php
+++ application/views/surveyresult.php
@@ -1,14 +1,28 @@
-
 <div class="container">
 	<div class="row">
-		<div class="col-sm-offset-2 col-sm-8" id="result-div">
-			<pre>
-
-<?php
-var_dump($survey);
-?>
-			</pre>
-			
+		<div class="col-sm-offset-2 col-sm-8" id="itemresult-div">
+			<ul>
+				<?php
+				// TODO: item sort after view 
+				foreach ($survey->items as $i => $item)
+				{
+					?>
+					<li>
+						<div class="row">
+							<div class="col-sm-6">
+								<h3>
+									<?= $item ?>
+								</h3>
+							</div>
+							<div class="col-sm-6">
+								<h3 class="num">
+									<?= $survey->result[$i] ?>票
+								</h3>
+							</div>
+						</div>
+					</li>
+				<?php } ?>
+			</ul>
 		</div>
 	</div>
 </div>
