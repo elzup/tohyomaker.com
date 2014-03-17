265c62564a3e202e27393c09fc57503b8f9bd34a
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-03-18 04:50:43 +0900

    update survey rsult view

--- application/views/surveylog.php
+++ application/views/surveylog.php
@@ -3,6 +3,7 @@
 	<div class="row">
 		<div class="col-sm-offset-2 col-sm-8">
 			<?php
+			// TODO: do folding logpane
 			foreach ($survey->results as $result)
 			{
 				logpane($result);
--- application/views/surveyresult.php
+++ application/views/surveyresult.php
@@ -11,35 +11,49 @@ if (!isset($select))
 			<div class="row">
 				<?php
 				// TODO: item sort after view 
+				$sum_cn = 0;
 				foreach ($survey->get_sorted() as $i => $item)
 				{
-					?>
-					<div class="col-sm-<?= calc_item_col($item->rank, count($survey->items)) ?>">
-						<div class="panel panel-default rank-<?= $item->rank ?>">
-							<div class="panel-body">
-								<div class="row">
-									<div class="col-sm-8">
-										<h4>
-											<?= $item->value ?>
-											<?php echo (($item->index == $select) ? '<i class="glyphicon glyphicon-ok"></i>' : '') ?>
-										</h4>
-									</div>
-									<div class="col-sm-4">
-										<h4 class="num">
-											<?= $item->num ?>
-										</h4>
-									</div>
+					$cn = calc_item_col($i, count($survey->items));
+					$voted_icon = (($item->index == $select) ? '<span class="voted" data-toggle="tooltip" data-placement="top" title="あなたが投票した項目"><i class="glyphicon glyphicon-flag"></i></span>' : '');
+					if ($sum_cn % 12 == 0)
+					{
+						?>
+						<div class="row">
+						<?php }
+						?>
+						<div class="col-sm-<?= $cn ?>">
+							<div class="panel panel-success rank-<?= $item->rank ?>">
+								<div class="panel-heading">
+									<span class="rank"><?= $item->rank ?></span>
+									<?= $voted_icon ?>
+								</div>
+								<div class="panel-body">
+									<h4>
+										<?= $item->value ?>
+									</h4>
+									<h4 class="num">
+										<?= $item->num ?>
+									</h4>
 								</div>
 							</div>
 						</div>
-					</div>
-				<?php } ?>
+						<?php
+						$sum_cn += $cn;
+						if ($sum_cn % 12 == 0)
+						{
+							?>
+						</div>
+						<?php
+					}
+				}
+				?>
 			</div>
 		</div>
 		<div class="col-sm-offset-2 col-sm-8" id="itemresultbox-div" style="display: none;">
 			<ul>
 				<?php
-				// TODO: item sort after view 
+// TODO: item sort after view 
 				foreach ($survey->items as $i => $item)
 				{
 					?>
