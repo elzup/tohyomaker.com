39b4876352ee5e1753379db50f196c219b24a618
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-03-23 22:47:12 +0900

    update navbar2

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -8,7 +8,7 @@ if ($user == null)
 ?>
 <nav class="navbar navbar-default navbar-fixed-top" id="navbar">
 	<div class="navbar-header">
-		<button class="navbar-toggle" data-toggle="collapse" data-target=".target">
+		<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-categlyes">
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
@@ -17,40 +17,27 @@ if ($user == null)
 		<div class="navbar-brand"><a <?= attr_href('') ?>>brand</a></div>
 		<!--<a href="" class="navbar-brand">投票メーカー</a>-->
 	</div>
-	<div class="collapse navbar-collapse target">
+	<div class="navbar-collapse collapse navbar-categlyes">
 		<ul class="nav navbar-nav navbar-right">
-			<li class="active">
-				<a <?= attr_href(HREF_TYPE_MAKE) ?>><?= tag_icon(ICON_MAKE) ?>作成</a>
-			</li>
-			<li>
-				<a <?= attr_href(HREF_TYPE_NEW) ?>><?= tag_icon(ICON_NEW) ?>新着</a>
-			</li>
-			<li>
-				<a <?= attr_href(HREF_TYPE_HOT) ?>><?= tag_icon(ICON_HOT) ?>人気</a>
-			</li>
-			<!--li>
-				<a onClick="$('#navbar').hide()">hide</a>
-			</li-->
-			<!--<li class="dropdown">-->
+			<li> <a <?= attr_href(HREF_TYPE_MAKE) ?>><?= tag_icon(ICON_MAKE) ?>作成</a> </li>
+			<li> <a <?= attr_href(HREF_TYPE_NEW) ?>><?= tag_icon(ICON_NEW) ?>新着</a> </li>
+			<li> <a <?= attr_href(HREF_TYPE_HOT) ?>><?= tag_icon(ICON_HOT) ?>人気</a> </li>
 			<?php
 			if ($user)
 			{
 				?>
-							<!--<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info"><?= $user->screen_name ?> <span class="caret"></span></a>-->
-				<!--<ul class="dropdown-menu" aria-labelledby="login-info">-->
 				<li>
-					<a <?= attr_href(HREF_TYPE_MYPAGE) ?>><?= tag_icon(ICON_HOME) ?><?= $user->screen_name ?></a>
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info"><?= tag_icon(ICON_USER) ?><span class="hidden-xs"><?= $user->screen_name ?> </span><span class="caret"></span></a>
+					<ul class="dropdown-menu" aria-labelledby="login-info">
+						<li><a <?= attr_href(HREF_TYPE_MYPAGE) ?>><?= tag_icon(ICON_HOME) ?><?= $user->screen_name ?></a></li>
+						<li><a <?= attr_href(HREF_TYPE_LOGOUT) ?>><?= tag_icon(ICON_LOGOUT) ?>ログアウト</a> </li>
+					</ul>
 				</li>
-				<!--li class="divider"></li-->
-				<li>
-					<a <?= attr_href(HREF_TYPE_LOGOUT). attr_tooltip('ログアウト') ?>><?= tag_icon(ICON_LOGOUT) ?></a>
-				</li>
-				<!--</ul>-->
 				<?php
 			} else
 			{
 				?>
-				<!--<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info"><?= tag_icon(ICON_LOGIN) ?>ログイン<span class="caret"></span></a>-->
+					<!--<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info"><?= tag_icon(ICON_LOGIN) ?>ログイン<span class="caret"></span></a>-->
 				<!--<ul class="dropdown-menu" aria-labelledby="login-info">-->
 				<li>
 					<a <?= attr_href(HREF_TYPE_LOGIN) ?>><?= tag_icon(ICON_TWITTER) ?>Twitter</a>
