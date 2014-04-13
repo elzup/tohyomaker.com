f5de754d7e28dd2ea5037406391b14daff4e910c
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-04-13 23:29:39 +0900

    update surveyhead view
    for smartphone view

--- application/views/surveyhead.php
+++ application/views/surveyhead.php
@@ -6,65 +6,53 @@
 
 <div class="container" id="survey-head-div">
 	<div class="well">
-		<!--div class="row">
-			<div class="survey-title" class="col-sm-8">
-				<h1><?= $survey->title ?></h1>
-			</div>
-			<div class ="col-sm-2" class="pagetype">投票ページ</div>
-		</div-->
-
 		<div class="row">
-			<div class="col-sm-5">
-				<div class="row">
-					<div class="col-sm-6 survey-cont timestamp">
-						<?= tag_icon(ICON_TIME, TRUE) ?>
-						<?= $survey->get_time() ?>
-					</div>
-					<div class="col-sm-6 owner-name">
-						<p>
-							<?php
-							if (!$survey->is_anonymous)
-							{
-								?>
-								<a <?= attr_href(HREF_TYPE_USER, $survey->owner->id) ?> class="btn btn-success btn-owner-name" <?= attr_tooltip("作者: {$survey->owner->screen_name}") ?> >
-									<?= tag_icon(ICON_USER) ?>@<?= $survey->owner->screen_name ?>
-								</a>
-								<?php
-							} else
-							{
-								?>
-								<a href="#" class="btn btn-default btn-owner-name " <?= attr_tooltip("作者非公開") ?>>
-									<?= tag_icon(ICON_USER) ?><?= NO_PARAM_STR ?>
-								</a>
-								<?php
-							}
-							?>
-						</p>
-					</div>
-				</div>
+			<div class="col-xs-8 survey-cont timestamp">
+				<?= tag_icon(ICON_TIME, TRUE) ?>
+				<?= $survey->get_time() ?>
+			</div>
+			<div class="col-xs-4 owner-name">
+				<?php
+				if (!$survey->is_anonymous)
+				{
+					?>
+					<?= tag_icon(ICON_USER, TRUE) ?>
+					<a <?= attr_href(HREF_TYPE_USER, $survey->owner->id) ?><?= attr_tooltip("作者: {$survey->owner->screen_name}") ?> >
+						@<?= $survey->owner->screen_name ?>
+					</a>
+					<?php
+				} else
+				{
+					?>
+					<a href="#" class="btn btn-default btn-owner-name " <?= attr_tooltip("作者非公開") ?>>
+						<?= tag_icon(ICON_USER) ?><?= NO_PARAM_STR ?>
+					</a>
+					<?php
+				}
+				?>
 			</div>
 		</div>
 
 		<div class="row">
-			<div class="col-sm-8 survey-cont tagbox">
-				<p class="btn-group-tags">
+			<div class="col-xs-8 survey-cont tagbox">
+				<div class="btn-group-tags">
 					<?= tag_icon(ICON_TAG, TRUE) ?>
 					<?php
 					foreach ($survey->tags as $tag)
 					{
 						?>
-						<a <?= attr_href(HREF_TYPE_TAG, $tag) ?> class="btn btn-success btn-tag btn-xs"><?= $tag ?></a>
+						<a <?= attr_href(HREF_TYPE_TAG, $tag) ?> class=""><?= $tag ?></a>
 					<?php } ?>
-				</p>
+				</div>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-sm-7 survey-cont" >
+			<div class="col-xs-8 survey-cont" >
 				<?php
 				if ($survey->state != SURVEY_STATE_END)
 				{
 					?>
-					<div class="col-sm-3 no-container" <?= attr_tooltip('集計まで残り時間') ?>>
+					<div class="col-xs-6 col-sm-3 no-container" <?= attr_tooltip('集計まで残り時間') ?>>
 						<?= tag_icon(ICON_FLAG, TRUE) ?>
 						<?= $survey->get_time_remain_str() ?>
 					</div>
@@ -72,7 +60,7 @@
 					$stylelib = explode(',', 'success,end,end');
 					$pbstyle = $stylelib[$survey->state];
 					?>
-					<div class="col-sm-9">
+					<div class="col-xs-6 col-sm-9">
 						<div class="progress">
 							<div class="progress-bar progress-bar-<?= $pbstyle ?>" style="width: <?= $survey->get_time_progress_par() ?>%;"></div>
 						</div>
@@ -91,7 +79,7 @@
 			<?= tag_icon(ICON_VOTE, TRUE) ?>
 				<span class="total-num"><?= $survey->total_num ?></span>
 			</div-->
-			<div class="col-sm-2 col-sm-offset-3 survey-cont">
+			<div class="col-xs-4 survey-cont">
 				<?php
 				$share_uri = fix_url(base_url(PATH_VOTE . $survey->id));
 				$share_text = totext_share_survey($survey);
@@ -103,20 +91,20 @@
 
 	<div class="row" id="survey-pager-div">
 
-		<div class="col-sm-12 col-sm-offset-0">
+		<div class="col-xs-12 col-xs-offset-0">
 			<div class="btn-group btn-group-justified">
-				<a <?= attr_href(HREF_TYPE_VOTE, $survey->id) ?> class="btn btn-success<?= (($type === SURVEY_PAGETYPE_VOTE) ? ' disabled' : '') ?>">
+				<a <?= attr_href(HREF_TYPE_VOTE, $survey->id) ?> class="btn btn-success<?= (($type === SURVEY_PAGETYPE_VOTE) ? ' disabled hidden-xs' : '') ?>">
 					<i class="<?= ICON_VOTE ?>"></i>
-					投票ページ
+					投票ページへ
 				</a>
-				<a <?= attr_href(HREF_TYPE_VIEW, $survey->id) ?> class="btn btn-success<?= (($type === SURVEY_PAGETYPE_VIEW) ? ' disabled' : '') ?>">
+				<a <?= attr_href(HREF_TYPE_VIEW, $survey->id) ?> class="btn btn-success<?= (($type === SURVEY_PAGETYPE_VIEW) ? ' disabled hidden-xs' : '') ?>">
 					<i class="<?= ICON_RESULT ?>"></i>
-					結果ページ
+					結果ページへ
 				</a>
 				<!--TODO: friend's vote page-->
-				<a <?= attr_href(HREF_TYPE_FRIEND, $survey->id) ?> class="btn btn-success<?= (($type === SURVEY_PAGETYPE_FRIEND) ? ' disabled' : '') ?>">
+				<a <?= attr_href(HREF_TYPE_FRIEND, $survey->id) ?> class="btn btn-success<?= (($type === SURVEY_PAGETYPE_FRIEND) ? ' disabled hidden-xs' : '') ?>">
 					<i class="<?= ICON_FRIEND ?>"></i>
-					フレンドの投票先
+					フレンドの投票先へ
 				</a>
 			</div>
 		</div>
@@ -124,12 +112,12 @@
 	<div class="row description">
 		<div class="col-sm-8 col-sm-offset-2">
 			<div class="well">
-			<?php
-			if (isset($survey->description))
-			{
-				?>
-						<p><span><?= $survey->description?></span></p>
-					</div>
+				<?php
+				if (isset($survey->description))
+				{
+					?>
+					<p><span><?= $survey->description ?></span></p>
+				</div>
 			<?php } ?>
 		</div>
 	</div>
