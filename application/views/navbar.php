3905ceace025ec7863584c42f92aa899cc8c12a0
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-04-18 16:01:55 +0900

    update navbar
    layout, img

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -16,7 +16,7 @@ if ($user == null)
 			if ($user->is_guest)
 			{
 				?>
-				<?= tag_icon(ICON_TWITTER); ?>
+				<?= tag_icon(ICON_LOGOUT); ?>
 				<?php
 			} else
 			{
@@ -31,16 +31,17 @@ if ($user == null)
 	</div>
 	<div class="navbar-collapse collapse navbar-categlyes">
 		<ul class="nav navbar-nav navbar-right">
-			<li> <a <?= attr_href(HREF_TYPE_MAKE) ?>><?= tag_icon(ICON_MAKE) ?>作成</a> </li>
-			<li> <a <?= attr_href(HREF_TYPE_NEW) ?>><?= tag_icon(ICON_NEW) ?>新着</a> </li>
-			<li> <a <?= attr_href(HREF_TYPE_HOT) ?>><?= tag_icon(ICON_HOT) ?>人気</a> </li>
+			<li> <a class="hidden-xs" <?= attr_href(HREF_TYPE_MAKE) ?>><?= tag_icon(ICON_MAKE) ?>作成</a> </li>
+			<li> <a class="hidden-xs" <?= attr_href(HREF_TYPE_NEW) ?>><?= tag_icon(ICON_NEW) ?>新着</a> </li>
+			<li> <a class="hidden-xs" <?= attr_href(HREF_TYPE_HOT) ?>><?= tag_icon(ICON_HOT) ?>人気</a> </li>
 
 			<?php
 			if (!$user->is_guest)
 			{
 				?>
 				<li>
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info"><?= tag_icon(ICON_USER) ?>
+					<a class="dropdown-toggle hidden-xs" data-toggle="dropdown" href="#" id="login-info">
+						<img class="user-icon" src="<?= $user->img_url ?>" alt="" />
 						<span class="hidden-xs"><?= $user->screen_name ?> </span>
 						<span class="caret"></span>
 					</a>
@@ -48,18 +49,25 @@ if ($user == null)
 						<li><a <?= attr_href(HREF_TYPE_MYPAGE) ?>><?= tag_icon(ICON_HOME) ?>マイページ</a></li>
 						<li><a <?= attr_href(HREF_TYPE_LOGOUT) ?>><?= tag_icon(ICON_LOGOUT) ?>ログアウト</a> </li>
 					</ul>
+
+				<li><a class="visible-xs" <?= attr_href(HREF_TYPE_MYPAGE) ?>><?= tag_icon(ICON_HOME) ?>マイページ</a></li>
+				<li><a class="visible-xs" <?= attr_href(HREF_TYPE_LOGOUT) ?>><?= tag_icon(ICON_LOGOUT) ?>ログアウト</a> </li>
+
 				</li>
 				<?php
 			} else
 			{
 				?>
 				<li>
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info"><!--<?= tag_icon(ICON_LOGIN) ?>-->ログイン<span class="caret"></span></a>
-					<ul class="dropdown-menu" aria-labelledby="login-info">
+					<a <?= attr_href(HREF_TYPE_LOGIN) ?>><?= tag_icon(ICON_TWITTER) ?>ログイン</a>
+					<!--a class="dropdown-toggle" data-toggle="dropdown" href="#" id="login-info">
+						<?= tag_icon(ICON_LOGIN) ?>ログイン<span class="caret"></span>
+					</a-->
+					<!--ul class="dropdown-menu" aria-labelledby="login-info">
 						<li>
 							<a <?= attr_href(HREF_TYPE_LOGIN) ?>><?= tag_icon(ICON_TWITTER) ?>Twitter</a>
 						</li>
-					</ul>
+					</ul-->
 				</li>
 			<?php } ?>
 
