ad5f2fa059affe04891c0aa856533a450c24aa47
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-04-21 22:16:47 +0900

    update logo design
    and header small fix

--- application/views/head.php
+++ application/views/head.php
@@ -14,10 +14,13 @@
     <link href='http://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0/build/cssreset/reset-min.css">
 
+		<!--<link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">-->
+
     <!-- Bootstrap -->
     <link rel="stylesheet" charset="UTF-8" href="<?= base_url('lib/bootstrap/css/bootstrap.min.css') ?>" media="screen" />
     <link rel="stylesheet" charset="UTF-8" href="<?= base_url('lib/bootstrap/css/font-awesome.min.css') ?>" media="screen" />
     <link rel="stylesheet/less" charset="UTF-8" type="text/css" href="<?= base_url('style/main.less') ?>" media="screen">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script> <![endif]-->
