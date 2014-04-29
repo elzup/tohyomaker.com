f8a6f9c590f40461fb83bab3dd94cda53d34646f
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-04-30 00:23:15 +0900

    update pagerobj

--- application/libraries/pagerobj.php
+++ application/libraries/pagerobj.php
@@ -2,10 +2,30 @@
 
 class Pagerobj {
 
-	public $current_index;
-	public $end_index;
+	// TODO: page_num, index, naming
+	public $index_current;
+	public $index_end;
+	public $width;
 
-	public function __construct($current_index)
+	public $current_num;
+
+	public $page_num;
+
+	public function __construct($start_num, $all_num, $width = 20)
+	{
+		$this->width = $width;
+		$this->index_end = floor ($all_num / $width) + 1;
+		$this->index_current = round ($start_num / $width) + 1;
+		$this->current_num;
+	}
+
+
+	public function is_last() 
+	{
+		
+	}
+
+	public function is_first()
 	{
 
 	}
